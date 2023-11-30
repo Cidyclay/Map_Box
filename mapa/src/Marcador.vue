a06c41dc68b86469b464d6ebdb3a89457f202bdd
Cidyclay <cidyclayextreme@gmail.com>
2023-11-30 00:10:28 -0300

    teste

---
+++ mapa/src/Marcador.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Marcador',
+    props: {
+      feature: Object
+    },
+    mounted() {
+      const { coordinates } = this.feature.geometry;
+  
+      new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .addTo(this.$parent.map);
+    }
+  };
+  </script>
+  
---
+++ mapa/src/Popup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="feature" class="popup">
+    <h3>{{ feature.properties.title }}</h3>
+    <p>{{ feature.properties.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popup',
+  props: {
+    feature: Object
+  }
+};
+</script>
+
+<style scoped>
+.popup {
+  position: absolute;
+  z-index: 10;
+  background: white;
+  padding: 10px;
+  border: 1px solid #ccc;
+}
+</style>
